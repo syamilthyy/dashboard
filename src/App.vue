27da9372d9125f5e9c6a03a8e57257465456b6d0
syamilthyy <syamilathiyya27@gmail.com>
2025-03-20 14:44:10 +0700

    add app.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <div class="flex flex-col justify-between min-h-screen">
+    <NavBar />
+    <p>Content</p>
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
